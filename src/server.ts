faecc347f4ae8080d1f8e550574c47dfa7d19a52
codlabsn <abdoulayeleom10@gmail.com>
2024-08-06 20:11:42 +0000

    add licence

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ const port = 3000;
 app.listen(3000, () =>
   console.log(`
 🚀 Server ready at:  http://${hostname}:${port} \n
-⭐️ Get a coffee and say hi to the creator of this project: https://twitter.com/danielroe \n
+⭐️ Get a coffee and say hi to the creator of this project: https://www.buymeacoffee.com/codelab_sn\n
 👤 Follow me on GitHub: https://github.com/abdoulayeDABO 👤`
 ));
+
